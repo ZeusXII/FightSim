9745efe24812c57d25f3b22b4440434aaea64a29
ZeusXII <55351262+ZeusXII@users.noreply.github.com>
2019-10-08 13:31:04 +0200

    descrpition
    Descrp

---
+++ FightSim/FightSim/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fighter A = new Fighter();
+            FighterB B = new FighterB();
+
+
+            Console.WriteLine("Fighter A: "+ A.PresentName() + " \n Fighter B: " + B.PresentName());
+            Console.WriteLine("Fighter A Swings at fihgter B, dealing " +  + "dmg");
+        
+
+            Console.ReadKey();
+        }
+    }
+}
